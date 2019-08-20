e1eb10705793e57f844b928d7a27553fabaeafb2
Arief Setya <ariefsetya@live.com>
2019-08-20 09:12:15 +0700

    add response code

--- server.js
+++ server.js
@@ -158,6 +158,15 @@ app.delete('/v2/user/:id',(req, res)=>{
 		})
 })
 
+app.get('/error-server',(req,res)=>{
+	res.send(500);
+})
+app.get('/bad-request',(req,res)=>{
+	res.send(400)
+})
+app.get('/created',(req,res)=>{
+	res.send(201);
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
